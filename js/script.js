9bc113998f772f70955866b9e3d328dddd57c951
RaphaelCarrah <raphaelscarrah95@gmail.com>
2017-01-26 12:11:08 -0300

    Adicionando arquivos do site

---
+++ js/script.js
@@ -0,0 +1,12 @@
+malarkey(document.querySelector('.breve'), {
+    typeSpeed: 50,
+    deleteSpeed: 50,
+    pauseDelay: 2000,
+    loop: true,
+    postfix: ''
+  })
+  .type('web').pause().delete()
+  .type('gráfico').pause().delete()
+  .type('3D').pause().delete()
+  .type('digital').pause().delete()
+ 
